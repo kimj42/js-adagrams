31c694a41d0c2c7bfea5398d23dd5a0d38df38ce
Karis Kim <ouniiq@hotmail.com>
2018-11-16 13:46:35 -0800

    all tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,57 +1,159 @@
 const Adagrams = {
   drawLetters() {
-   const letter_bag = [
-     "A", "A", "A", "A", "A", "A", "A", "A", "A",
-     "B", "B",
-     "C", "C",
-     "D", "D", "D", "D",
-     "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-     "F", "F",
-     "G", "G", "G",
-     "H", "H",
-     "I", "I", "I", "I", "I", "I", "I", "I", "I",
-     "J",
-     "K",
-     "L", "L", "L", "L",
-     "M", "M",
-     "N", "N", "N", "N", "N", "N",
-     "O", "O", "O", "O", "O", "O", "O", "O",
-     "P", "P",
-     "Q",
-     "R", "R", "R", "R", "R", "R",
-     "S", "S", "S", "S",
-     "T", "T", "T", "T", "T", "T",
-     "U", "U", "U", "U",
-     "V", "V",
-     "W", "W",
-     "X",
-     "Y", "Y",
-     "Z"]
+    const letter_bag = [
+      "A", "A", "A", "A", "A", "A", "A", "A", "A",
+      "B", "B",
+      "C", "C",
+      "D", "D", "D", "D",
+      "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+      "F", "F",
+      "G", "G", "G",
+      "H", "H",
+      "I", "I", "I", "I", "I", "I", "I", "I", "I",
+      "J",
+      "K",
+      "L", "L", "L", "L",
+      "M", "M",
+      "N", "N", "N", "N", "N", "N",
+      "O", "O", "O", "O", "O", "O", "O", "O",
+      "P", "P",
+      "Q",
+      "R", "R", "R", "R", "R", "R",
+      "S", "S", "S", "S",
+      "T", "T", "T", "T", "T", "T",
+      "U", "U", "U", "U",
+      "V", "V",
+      "W", "W",
+      "X",
+      "Y", "Y",
+      "Z"]
 
-   letter_bag.sort(() => Math.random() - 0.5);
-   return letter_bag.slice(0, 10);
-  },
+      letter_bag.sort(() => Math.random() - 0.5);
+      return letter_bag.slice(0, 10);
+    },
 
-  usesAvailableLetters(input, letters_in_hand) {
-    const inputArray = input.toUpperCase().split("");
-    let letter_hash = {}
+    usesAvailableLetters(input, letters_in_hand) {
+      const inputArray = input.toUpperCase().split("");
+      let letter_hash = {};
 
-    letters_in_hand.forEach(function(letter) {
-      if (letter_hash[letter]) {
-        letter_hash[letter] += 1;
+
+      letters_in_hand.forEach((letter) => {
+        if (letter_hash[letter]) {
+          letter_hash[letter] += 1;
+        }
+        else {
+          letter_hash[letter] = 1;
+        }
+      });
+
+      for (let input of inputArray) {
+        if (letter_hash[input] < 1) {
+          return false;
+        }
+        else if (letter_hash[input] >= 1) {
+            letter_hash[input] -= 1;
+          }
+          else {
+            return false;
+          }
+      }
+      return true;
+    },
+
+    scoreWord(input) {
+      const letterScores = {
+      A: 1,
+      E: 1,
+      I: 1,
+      O: 1,
+      U: 1,
+      L: 1,
+      N: 1,
+      R: 1,
+      S: 1,
+      T: 1,
+      D: 2,
+      G: 2,
+      B: 3,
+      C: 3,
+      M: 3,
+      P: 3,
+      F: 4,
+      H: 4,
+      V: 4,
+      W: 4,
+      Y: 4,
+      K: 5,
+      J: 8,
+      X: 8,
+      Q: 10,
+      Z: 10
+    };
+
+    let totalScore = 0;
+
+    const letters = input.toUpperCase().split("");
+
+    for (let letter of letters) {
+      totalScore += letterScores[letter];
+    }
+
+    totalScore += letters.length > 6 ? 8 : 0
+
+    return totalScore;
+    },
+
+    breakTie(incumbent, challenger) {
+      if (incumbent.word.length == 10) {
+        return incumbent;
+      }
+      else if (challenger.word.length == 10) {
+        return challenger;
+      }
+      else if (challenger.word.length < incumbent.word.length) {
+        return challenger;
       }
       else {
-        letter_hash[letter] = 1;
+        return incumbent;
       }
-    })
+    },
 
-  },
-};
+    highestScoreFrom(words) {
+      // console.log(words);
+      const scoreHashes = [];
 
-// Do not remove this line or your tests will break!
-export default Adagrams;
+      // for (let word of words) {
+      //   // console.log(scoreHashes);
+      //   scoreHashes["word"] = word;
+      //   scoreHashes["score"] = this.scoreWord(word);
+      // }
+
+      for (let word of words) {
+        // console.log(scoreHashes);
+        scoreHashes.push({word: word, score: this.scoreWord(word)});
+      }
+
+      // console.log(scoreHashes);
+      let winningHash = scoreHashes[0];
+      // console.log(winningHash);
+      for (let currentHash of scoreHashes) {
+        if (currentHash.score > winningHash.score) {
+          winningHash = currentHash;
+        }
+        else if (currentHash.score == winningHash.score) {
+          winningHash = this.breakTie(winningHash, currentHash);
+        }
+      }
+
+      return winningHash;
+    },
+  }
 
 
+  // Do not remove this line or your tests will break!
+  export default Adagrams;
+
+//
 //  const alphabetHash = {
 //   A: 9, B: 2,
 //   C: 2, D: 4,
